c60ed008bfac261733711d1207cb0b3e4278daf9
Laurie Lee Moses <lmoses@colum.edu>
2013-02-11 11:08:01 -0600

    changed stuff in gemfile for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = 'b4b75a44f712cf5c4c4a2ec0a5a6d266046132e60538ebed9668c63343c10c10267257dba09d9002286853bacf96c825d6e0cc9e95c1906db4f523b620180ea0'
